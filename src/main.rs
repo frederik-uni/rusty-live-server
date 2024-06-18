7010a49571e3bc7748f82c11c2009653a62226ff
frederik <termite_pilgrim04@icloud.com>
2024-06-18 22:41:24 +0200

    add cli

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,34 @@
+#[cfg(feature = "binary")]
 use std::path::PathBuf;
 
+#[cfg(feature = "binary")]
+use clap::{Parser, Subcommand};
+
+#[cfg(feature = "binary")]
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long, value_name = "PORT", default_value_t = 8080)]
+    port: u16,
+    #[command(subcommand)]
+    command: Commands,
+}
+#[cfg(feature = "binary")]
+#[derive(Subcommand)]
+enum Commands {
+    /// PATH
+    Serve { path: PathBuf },
+}
+
+#[cfg(feature = "binary")]
 #[tokio::main]
 async fn main() {
-    serve_dir::serve(PathBuf::from("./html"), 8081, true, None)
-        .await
-        .unwrap();
+    let cli = Cli::parse();
+    let Commands::Serve { path } = cli.command;
+    serve_dir::serve(path, cli.port, true, None).await.unwrap();
+}
+
+#[cfg(not(feature = "binary"))]
+fn main() {
+    println!("Binary Feature required")
 }
