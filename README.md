1dba18d1bba9bacf48a76bc8b7323b31137d4891
frederik <termite_pilgrim04@icloud.com>
2024-06-19 13:12:28 +0200

    remove clap & readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Rusty Live Server
+
+```sh
+rusty-live-server serve ./html --port 8080
+```
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,59 @@
-#[cfg(feature = "binary")]
-use std::path::PathBuf;
+use std::{env, path::PathBuf};
 
-#[cfg(feature = "binary")]
-use clap::{Parser, Subcommand};
+const HELP: &str = "Usage: rusty-live-server PATH [OPTIONS]\n\nOptions:\n  -p, --port <PORT>  [default: 8080]\n  -h, --help         Print help\n  -V, --version      Print version";
 
-#[cfg(feature = "binary")]
-#[derive(Parser)]
-#[command(version, about, long_about = None)]
-struct Cli {
-    #[arg(short, long, value_name = "PORT", default_value_t = 8080)]
-    port: u16,
-    #[command(subcommand)]
-    command: Commands,
-}
-#[cfg(feature = "binary")]
-#[derive(Subcommand)]
-enum Commands {
-    /// PATH
-    Serve { path: PathBuf },
-}
-
-#[cfg(feature = "binary")]
 #[tokio::main]
 async fn main() {
-    let cli = Cli::parse();
-    let Commands::Serve { path } = cli.command;
-    serve_dir::serve(path, cli.port, true, None).await.unwrap();
+    match parse_args() {
+        Ok((path, port)) => rusty_live_server::serve(path, port, true, None)
+            .await
+            .unwrap(),
+        Err(msg) => {
+            println!("{msg}")
+        }
+    }
 }
 
-#[cfg(not(feature = "binary"))]
-fn main() {
-    println!("Binary Feature required")
+fn parse_args() -> Result<(PathBuf, u16), &'static str> {
+    let mut args = env::args().skip(1);
+    let mut path = vec![];
+    let mut port: u16 = 8080;
+    let mut next_port = false;
+
+    while let Some(arg) = args.next() {
+        if let Some(arg) = arg.strip_prefix("-") {
+            if next_port {
+                return Err("error: a value is required for '--port <PORT>' but none was supplied");
+            }
+            match arg {
+                "-port" | "p" => {
+                    next_port = true;
+                }
+                "-version" | "V" => return Err("rusty-live-server 0.4.0"),
+                "-help" | "h" => return Err(HELP),
+                _ => {
+                    return Err("unkown argument");
+                }
+            };
+        } else {
+            match next_port {
+                true => {
+                    next_port = false;
+                    port = arg.parse().map_err(|_| "Invalid port")?;
+                }
+                false => path.push(arg),
+            }
+        }
+    }
+    if next_port {
+        return Err("error: a value is required for '--port <PORT>' but none was supplied");
+    }
+    if path.is_empty() {
+        return Err(HELP);
+    }
+    if path.len() > 1 {
+        return Err("error: more than 1 value provided");
+    }
+
+    Ok((PathBuf::from(path.pop().unwrap()), port))
 }
